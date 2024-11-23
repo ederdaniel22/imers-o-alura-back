90c43b8640a047cdd60aab5a8a5995f76771366b
Eder Daniel Domingues <eder.domingues@gmail.com>
2024-11-23 16:55:49 -0400

    Inserindo imagens e fazendo uploads

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listarPosts(req, res){// Chama a função para obter os posts
     const posts = await getTodosPosts();
@@ -10,6 +11,23 @@ export async function postarNovoPost(req, res){
     const novoPost = req.body;
     try{
         const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+} catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({"Erro": "Falha na requisição"})
+}
+  }
+export async function uploadImagem(req, res){
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalmente,
+        alt: ""
+    };
+    
+    try{
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
         res.status(200).json(postCriado);
 } catch (erro) {
     console.error(erro.message);
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,13 +1,27 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem} from "../controllers/postsController.js";
 
- const routes = (app) => {
-    // Habilita o parsing de JSON nas requisições
-    app.use(express.json());
-    // Rota GET para obter todos os posts
-    app.get("/posts", listarPosts);
-    // Rota para criar posts
-    app.post("/posts", postarNovoPost)
+// Configuração no Windows para escrever corretamente o nome dos objetos
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.originalname);
+  }
+})
+// Até aqui, se for no linux só precisa da linha abaixo sem o storage
+const upload = multer({ dest: "./uploads" , storage})
+
+const routes = (app) => {
+   // Habilita o parsing de JSON nas requisições
+   app.use(express.json());
+   // Rota GET para obter todos os posts
+   app.get("/posts", listarPosts);
+   // Rota para criar posts
+   app.post("/posts", postarNovoPost);
+   app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 export default routes;
 
